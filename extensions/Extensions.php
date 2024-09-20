61d2a5f6bb54e1dd908f6cfb6211ea415a5d1078
Alex <puyaalexxx@gmail.com>
2024-09-20 21:12:20 +0300

    removed some error_log calls

--- extensions/Extensions.php
+++ extensions/Extensions.php
@@ -3,7 +3,9 @@ declare( strict_types = 1 );
 
 namespace DHT\Extensions;
 
-if( !defined( 'DHT_MAIN' ) ) die( 'Forbidden' );
+if ( ! defined( 'DHT_MAIN' ) ) {
+	die( 'Forbidden' );
+}
 
 use DHT\Extensions\CPT\{CPT, ICPT};
 use DHT\Extensions\DashPages\{DashMenuPage, IDashMenuPage};
@@ -11,9 +13,9 @@ use DHT\Extensions\Options\{IOptions, Options};
 use DHT\Extensions\Sidebars\{CreateDynamicSidebars, ICreateDynamicSidebars, IRegisterSidebar, RegisterSidebar};
 use DHT\Extensions\Widgets\{IRegisterWidget, RegisterWidget};
 use DHT\Helpers\Exceptions\ConfigExceptions\{EmptyCPTConfigurationsException,
-    EmptyMenuConfigurationsException,
-    EmptySidebarConfigurationsException,
-    EmptyWidgetNamesException
+	EmptyMenuConfigurationsException,
+	EmptySidebarConfigurationsException,
+	EmptyWidgetNamesException
 };
 use DHT\Helpers\Traits\{SingletonTrait, ValidateConfigurations};
 use function DHT\Helpers\dht_print_r;
@@ -22,112 +24,108 @@ use function DHT\Helpers\dht_print_r;
  * Singleton Class that is used to include all the framework extensions and initialise them
  */
 final class Extensions {
-    
-    use ValidateConfigurations;
-    use SingletonTrait;
-    
-    private $id;
-    
-    /**
-     * @since     1.0.0
-     */
-    private function __construct() {
-        
-        do_action( 'dht_before_extensions_init' );
-        
-        $this->id = uniqid();
-        
-        error_log( 'Extensions Instance ID: ' . $this->id );
-    }
-    
-    /**
-     * get dashboard menus extension class instance
-     *
-     * @param array $dash_menus_config - dashboard menus configurations
-     *
-     * @return IDashMenuPage - menu instance
-     * @since     1.0.0
-     */
-    public function dashmenus( array $dash_menus_config ) : IDashMenuPage {
-        
-        $dash_menus_config = $this->_validateConfigurations( $dash_menus_config, '', 'dash_menus_configurations', EmptyMenuConfigurationsException::class, _x( 'Empty dashboard menu configurations array provided', 'exceptions', DHT_PREFIX ) );
-        
-        return new DashMenuPage( $dash_menus_config );
-    }
-    
-    /**
-     * get custom post types extension class instance
-     *
-     * @param array $cpt_config
-     *
-     * @return ICPT - cpt instance
-     * @since     1.0.0
-     */
-    public function cpts( array $cpt_config ) : ICPT {
-        
-        $cpt_config = $this->_validateConfigurations( $cpt_config, '', 'dht_cpts_configurations', EmptyCPTConfigurationsException::class, _x( 'Empty cpt configurations array provided', 'exceptions', DHT_PREFIX ) );
-        
-        return new CPT( $cpt_config );
-    }
-    
-    /**
-     * get options extension class instance
-     *
-     * @param array $options
-     *
-     * @return IOptions|null - options instance
-     * @since     1.0.0
-     */
-    public function options( array $options ) : ?IOptions {
-        
-        $options = $this->_validateConfigurations( $options, '', 'dht_options_configurations' );
-        
-        //if the options exists or if it is an ajax request
-        // (needed for the options that use ajax specifically)
-        if( !( !empty( $options ) || wp_doing_ajax() ) ) return NULL;
-        
-        return new Options( $options );
-    }
-    
-    /**
-     * get widgets extension class instance
-     *
-     * @param array $widgets_config
-     *
-     * @return IRegisterWidget - register widgets class instance
-     * @since     1.0.0
-     */
-    public function widgets( array $widgets_config ) : IRegisterWidget {
-        
-        $widgets_config = $this->_validateConfigurations( $widgets_config, '', 'dht_widgets_configurations', EmptyWidgetNamesException::class, _x( 'Empty widgets configurations array provided', 'exceptions', DHT_PREFIX ) );
-        
-        return new RegisterWidget( $widgets_config );
-    }
-    
-    /**
-     * get sidebars extension class instance
-     *
-     * @param array $sidebar_config
-     *
-     * @return IRegisterSidebar - register sidebar class instance
-     * @since     1.0.0
-     */
-    public function sidebars( array $sidebar_config ) : IRegisterSidebar {
-        
-        $sidebar_config = $this->_validateConfigurations( $sidebar_config, '', 'dht_sidebars_configurations', EmptySidebarConfigurationsException::class, _x( 'Empty configurations array provided', 'exceptions', DHT_PREFIX ) );
-        
-        return new RegisterSidebar( $sidebar_config );
-    }
-    
-    /**
-     * get dynamic sidebars extension class instance
-     *
-     * @return ICreateDynamicSidebars - create sidebar class instance
-     * @since     1.0.0
-     */
-    public function dynamicSidebars() : ICreateDynamicSidebars {
-        
-        return new CreateDynamicSidebars();
-    }
-    
+	
+	use ValidateConfigurations;
+	use SingletonTrait;
+	
+	/**
+	 * @since     1.0.0
+	 */
+	private function __construct() {
+		
+		do_action( 'dht_before_extensions_init' );
+	}
+	
+	/**
+	 * get dashboard menus extension class instance
+	 *
+	 * @param array $dash_menus_config - dashboard menus configurations
+	 *
+	 * @return IDashMenuPage - menu instance
+	 * @since     1.0.0
+	 */
+	public function dashmenus( array $dash_menus_config ) : IDashMenuPage {
+		
+		$dash_menus_config = $this->_validateConfigurations( $dash_menus_config, '', 'dash_menus_configurations', EmptyMenuConfigurationsException::class, _x( 'Empty dashboard menu configurations array provided', 'exceptions', DHT_PREFIX ) );
+		
+		return new DashMenuPage( $dash_menus_config );
+	}
+	
+	/**
+	 * get custom post types extension class instance
+	 *
+	 * @param array $cpt_config
+	 *
+	 * @return ICPT - cpt instance
+	 * @since     1.0.0
+	 */
+	public function cpts( array $cpt_config ) : ICPT {
+		
+		$cpt_config = $this->_validateConfigurations( $cpt_config, '', 'dht_cpts_configurations', EmptyCPTConfigurationsException::class, _x( 'Empty cpt configurations array provided', 'exceptions', DHT_PREFIX ) );
+		
+		return new CPT( $cpt_config );
+	}
+	
+	/**
+	 * get options extension class instance
+	 *
+	 * @param array $options
+	 *
+	 * @return IOptions|null - options instance
+	 * @since     1.0.0
+	 */
+	public function options( array $options ) : ?IOptions {
+		
+		$options = $this->_validateConfigurations( $options, '', 'dht_options_configurations' );
+		
+		//if the options exists or if it is an ajax request
+		// (needed for the options that use ajax specifically)
+		if ( ! ( ! empty( $options ) || wp_doing_ajax() ) ) {
+			return NULL;
+		}
+		
+		return new Options( $options );
+	}
+	
+	/**
+	 * get widgets extension class instance
+	 *
+	 * @param array $widgets_config
+	 *
+	 * @return IRegisterWidget - register widgets class instance
+	 * @since     1.0.0
+	 */
+	public function widgets( array $widgets_config ) : IRegisterWidget {
+		
+		$widgets_config = $this->_validateConfigurations( $widgets_config, '', 'dht_widgets_configurations', EmptyWidgetNamesException::class, _x( 'Empty widgets configurations array provided', 'exceptions', DHT_PREFIX ) );
+		
+		return new RegisterWidget( $widgets_config );
+	}
+	
+	/**
+	 * get sidebars extension class instance
+	 *
+	 * @param array $sidebar_config
+	 *
+	 * @return IRegisterSidebar - register sidebar class instance
+	 * @since     1.0.0
+	 */
+	public function sidebars( array $sidebar_config ) : IRegisterSidebar {
+		
+		$sidebar_config = $this->_validateConfigurations( $sidebar_config, '', 'dht_sidebars_configurations', EmptySidebarConfigurationsException::class, _x( 'Empty configurations array provided', 'exceptions', DHT_PREFIX ) );
+		
+		return new RegisterSidebar( $sidebar_config );
+	}
+	
+	/**
+	 * get dynamic sidebars extension class instance
+	 *
+	 * @return ICreateDynamicSidebars - create sidebar class instance
+	 * @since     1.0.0
+	 */
+	public function dynamicSidebars() : ICreateDynamicSidebars {
+		
+		return new CreateDynamicSidebars();
+	}
+	
 }
--- helpers/traits/SingletonTrait.php
+++ helpers/traits/SingletonTrait.php
@@ -3,58 +3,56 @@ declare( strict_types = 1 );
 
 namespace DHT\Helpers\Traits;
 
-if( !defined( 'DHT_MAIN' ) ) die( 'Forbidden' );
+if ( ! defined( 'DHT_MAIN' ) ) {
+	die( 'Forbidden' );
+}
 
 use Exception;
 
 trait SingletonTrait {
-    
-    /**
-     * Holds the single instance of the class.
-     *
-     * @var array
-     */
-    private static array $_instances = [];
-    
-    /**
-     * This is the static method that controls the access to the singleton
-     * instance. On the first run, it creates a singleton object and places it
-     * into the static field. On subsequent runs, it returns the existing
-     * object stored in the static field.
-     *
-     * @return self - current class
-     * @since     1.0.0
-     */
-    public static function init() : self {
-        
-        $cls = static::class;
-        if( !isset( self::$_instances[ $cls ] ) ) {
-            error_log( "Creating new instance of {$cls}" );
-            self::$_instances[ $cls ] = new static();
-        }
-        else {
-            error_log( "Returning existing instance of {$cls}" );
-        }
-        
-        return self::$_instances[ $cls ];
-    }
-    
-    /**
-     * Prevents cloning of the instance.
-     *
-     * @since     1.0.0
-     */
-    protected function __clone() : void {}
-    
-    /**
-     * Prevents unserializing of the instance.
-     *
-     * @throws Exception
-     * @since     1.0.0
-     */
-    public function __wakeup() {
-        
-        throw new Exception( _x( 'Cannot unserialize singleton', 'traits', DHT_PREFIX ) );
-    }
-    
+	
+	/**
+	 * Holds the single instance of the class.
+	 *
+	 * @var array
+	 */
+	private static array $_instances = [];
+	
+	/**
+	 * This is the static method that controls the access to the singleton
+	 * instance. On the first run, it creates a singleton object and places it
+	 * into the static field. On subsequent runs, it returns the existing
+	 * object stored in the static field.
+	 *
+	 * @return self - current class
+	 * @since     1.0.0
+	 */
+	public static function init() : self {
+		
+		$cls = static::class;
+		if ( ! isset( self::$_instances[ $cls ] ) ) {
+			self::$_instances[ $cls ] = new static();
+		}
+		
+		return self::$_instances[ $cls ];
+	}
+	
+	/**
+	 * Prevents cloning of the instance.
+	 *
+	 * @since     1.0.0
+	 */
+	protected function __clone() : void {}
+	
+	/**
+	 * Prevents unserializing of the instance.
+	 *
+	 * @throws Exception
+	 * @since     1.0.0
+	 */
+	public function __wakeup() {
+		
+		throw new Exception( _x( 'Cannot unserialize singleton', 'traits', DHT_PREFIX ) );
+	}
+	
 }

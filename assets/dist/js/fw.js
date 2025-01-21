a012b55a8ee6e4441ea3baf4c243e50cac4479ec
Alex <puyaalexxx@gmail.com>
2025-01-21 20:08:47 +0200

    added dist folder to git

---
+++ assets/dist/js/fw.js
@@ -0,0 +1,1 @@
+import{dhtInitializePreloader as n}from"./preloader.js";(function(i){class t{constructor(){this.init()}init(){n()}}i(function(){new t})})(jQuery);
---
+++ assets/dist/js/general.js
@@ -0,0 +1,1 @@
+function e(o,r=""){r.length?console.error(`Error loading module ${r}:`,o):console.error("Error loading module:",o)}export{e};
